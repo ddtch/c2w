472779ec9cc22415a8a7456de939d5fdb573d8fb
dd <4daveid@gmail.com>
2022-05-04 19:39:04 -0700

    initial commit

---
+++ src/components/TasksFeed.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class TasksFeed extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TasksFeed</Text>
+      </View>
+    )
+  }
+}
+
+export default TasksFeed
---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,16 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class MainScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MainScreen</Text>
+      </View>
+    )
+  }
+}
+
+const mainScreenStyles = StyleSheet.create({
+
+})
